ff9c59de590bbf6291a84404dbf75a12555d7656
LazyRenjen <renjensherpa10@gmail.com>
2024-04-14 21:19:38 +0545

    Sprint 1 backend program

--- routes/pages.js
+++ routes/pages.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const app = express();
+app.use(express.static('public')); // 'public' is the directory name where images are stored
+
 router.get('/', (req, res) => {
     res.render("login");
 });
